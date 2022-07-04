526ef9fc518ba82b481c844643756e3b98312faf
hzq <hzq_666@163.com>
2022-07-04 18:10:48 +0800

    2022/07/04

--- exercise/计算器.py
+++ exercise/计算器.py
@@ -1,24 +1,34 @@
-from math import *
-try:
-    from tkinter import *
-except ImportError as e:
-    print(e)
-def calc():
-    text = var.get()
-    result = eval(text)
-    result = result / 1.0
-    result = str(result)
-    var.set(result)
-text = ''
-result = ''
-root = Tk()
-pi = 3.1415926535897932
-root.geometry('360x48')
-root.resizable(width=False,height=False)
-root.title('Mini Calculator')
-var = StringVar()
-entry = Entry(root,textvariable=var,width=360)
-button = Button(root,text='GET RESULT',command=calc)
-entry.pack()
-button.pack()
-root.mainloop()
+class oper:
+    oper = ''
+    func = ''
+
+    
+    def __init__(self,oper):
+        self.oper = oper.strip()
+
+    def opers(self,num1,num2):
+        switcher = {
+            "+":"add",
+            "-":"substract",
+            "*":"multiply",
+            "/":"divide"
+        }
+        func = switcher.get(self.oper)
+        pass
+
+    def add(self,num1,num2):
+        return num1+num2
+
+    def subtract(self,num1,num2):
+        return num1-num2
+    
+    def multiply(self,num1,num2):
+        return num1*num2
+
+    def divide(self,num1,num2):
+        return num1/num2
+    
+
+    
+
+    
