0f244bd21e37b3fe705d7ecc5c61050fcac75c5f
hzq <hzq_666@163.com>
2022-07-06 17:45:06 +0800

    update

--- exercise/约瑟夫.py
+++ exercise/约瑟夫.py
@@ -1,26 +1,9 @@
-people={}
-for x in range(1,31):
-    people[x]=1
-# print(people)
-check=0
-i=1
-j=0
-while i<=31:
-    if i == 31:
-        i=1
-    elif j == 15:
-        break
-    else:
-        if people[i] == 0:
-            i+=1
-            continue
-        else:
-            check+=1
-            if check == 9:
-                people[i]=0
-                check = 0
-                print("{}号下船了".format(i))
-                j+=1
-            else:
-                i+=1
-                continue
+li  = list(range(1,31))
+count  = 0
+
+while count <15:
+    
+    print("{}号下船".format(li[8]))
+    li = li[9:]+li[:8]
+    count+=1
+
