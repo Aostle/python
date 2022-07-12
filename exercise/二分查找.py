689cb918afed7b4b3b70d8f0b3da6f0b364a84a1
hzq <hzq_666@163.com>
2022-07-12 17:39:14 +0800

    update

--- exercise/二分查找.py
+++ exercise/二分查找.py
@@ -19,16 +19,30 @@ def binarySearch(lst:list,num):
 
     return -1
 
-def recursionSearch(lst:list,num):
-    
-    pass
+def recursionSearch(lst:list,l,r,num):
+    if(l<=r):
+        mid = (l+r)//2
+        if lst[mid] == num:
+            return mid
+        if num< lst[mid]:
+            return recursionSearch(lst,l,mid-1,num)
+        if num> lst[mid]:
+            return recursionSearch(lst,mid+1,r,num)
+    else:
+        return -1
+
+def linearSearch(arr,x):
+    for i in range(0,len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
 
 
 # 测试数组(必须是顺序数组)
-arr = [2, 3, 10, 14, 40]
-x = 40
+arr = [i for i in range(1,10100)]
+x = 888
 
-result = binarySearch(arr,x)
+result = linearSearch(arr,x)
 
 if result!=-1:
     print(f'元素{x}在数组中下标为:',result)
